9f6d7a6eabac063a2fd3944a97c8eb60268c2a77
unknown <ifc@campus.concordia.ifc.edu.br>
2024-11-13 14:13:22 -0300

    corrigi a mudança

--- script/script.js
+++ script/script.js
@@ -1,1 +1,29 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+const totalSlides = slides.length;
+const indicators = document.querySelectorAll('.carousel-indicators div');
 
+function updateCarousel() {
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel').style.transform = `translateX(${offset}%)`; }
+    
+function moveSlide(direction) {
+    currentIndex += direction;
+
+    if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    } else if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    }
+
+    updateCarousel();
+}
+
+function moveToSlide(index) {
+    currentIndex = index;
+    updateCarousel();
+}
+
+setInterval(() => {
+    moveSlide(1);
+}, 4000); 
