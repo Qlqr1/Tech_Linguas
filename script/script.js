18dc57329cc660f5f7b667a39f9d416f7c529ef9
Qlqr1 <guto.kg2008@gmail.com>
2025-01-08 01:42:10 -0300

    Trocar idioma implementado

--- script/script.js
+++ script/script.js
@@ -29,4 +29,50 @@ setInterval(() => {
     moveSlide(1);
 }, 4000); 
 
-// Link interno
+// Trocar Idioma
+const rbPortugues = document.getElementById("rbPortugues");
+const rbIngles = document.getElementById("rbIngles");
+const rbEspanhol = document.getElementById("rbEspanhol");
+
+let inputsRadio = document.getElementsByTagName("input");
+for (let i = 0; i < inputsRadio.length; i++) { 
+    inputsRadio[i].addEventListener("change", trocarIdioma); 
+}
+
+function trocarIdioma(){
+    var idioma;
+    if (rbPortugues.checked){
+        idioma = "pt";
+    } else if(rbIngles.checked){
+        idioma = "en";
+    } else if(rbEspanhol.checked){
+        idioma = "es";
+    }
+
+    localStorage.setItem("idioma", idioma);
+
+    const elements = document.querySelectorAll('[data-lang-pt]');
+    elements.forEach(element => {
+    const translation = element.getAttribute(`data-lang-${idioma}`);
+    if (translation) {
+        element.textContent = translation;
+    }
+  });    
+}
+
+function verificarIdioma() { 
+    if (localStorage.getItem("idioma")) {
+        let idioma = localStorage.getItem("idioma");
+        if (idioma == "pt") {
+            rbPortugues.checked = true; 
+        } else if (idioma == "en") {
+            rbIngles.checked = true;
+        } else if(idioma == "es"){ 
+            rbEspanhol.checked = true; 
+        }
+        trocarIdioma();
+    }
+    
+}
+
+verificarIdioma();
